59d0b4a5b84fb6d750a7c2680cb1e99984de4ced
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2025-12-02 22:02:35 +0530

    Implement basic bitwise operations in 2dec.cpp

---
+++ 2dec.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // int a,b;
+  // cout<<"Enter the first number:";
+  // cin>>a;
+  // cout<<"Enter the second number:";
+  // cin>>b;
+
+
+  // cout<<"Your sum is:"<<a+b;
+
+
+  // char c = 'a'+1;;
+  // cout<<c;
+  // cout<<sizeof(c);
+
+  // arithmetic operator   +,-,*,/,%
+
+
+  // int a = 20;
+  // int b = 10;
+
+  // cout<<a+b<<endl;
+  // cout<<a-b<<endl;
+  // cout<<a*b<<endl;
+  // cout<<a/b<<endl;
+  // cout<<a%b<<endl;
+
+  // assignment operator
+
+  // a+=b;
+  // a = a + b; 
+
+  // a=-b;
+  // // a = a - b;
+
+  // a*=b;
+  // // a = a*b;
+
+  // a/=b;
+  // a%=b;
+  // cout<<a;
+
+  // comparison operator >,<,>=,<=,!=,==
+
+  // int a = 20;
+  // int b = 10;
+
+  // cout<<(a==b);
+  // int a=20;
+
+  // // // left shift operator
+  // // a = a<<2;
+
+  // //Right shift
+  // a = a>>2;
+  // cout<<a;
+
+  // cout<<(true&&true)<<endl; and  both should be true
+  // cout<<(true&&0);
+
+  // cout<<(true||true)<<endl; or  if both side false then only false otherwise true
+  // cout<<(0||0);
+
+  // cout<<(!false);  not   if true then false and if false then true
+
+  // pre-incremenet,post
+  // decrement pre and post
+
+  // int a =20;
+  // int b = a++;
+
+  // cout<<b<<endl;
+  // cout<<a;
+
+  // int a =20;
+  // int b = --a;
+
+  // cout<<b<<endl;
+  // cout<<a;
+
+
+  //bit wise operator
+  //  & | ^  and, or, xor   operations on bits of these numbers
+  int a = 10;
+  int b  = 5;
+
+  cout<<(a ^ b);
+
+
+
+
+
+  return 0;
+}

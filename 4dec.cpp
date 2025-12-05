a864a005810529210287e269d034e1b52ec9171c
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2025-12-05 17:45:10 +0530

    Add initial implementation of 4dec.cpp with commented code examples

---
+++ 4dec.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Even number
+  
+  // for (int i = 0; i < 20; i=i+2)
+  // {
+  //   cout<<i<<endl;
+  // }
+
+
+
+  // odd number
+
+  // for(int i = 1; i < 20; i+=2)
+  // {
+  //   cout<<i<<endl;
+  // }
+  
+  // a to z
+
+  // for (int i = 97; i < 123; i++)
+  // {
+  //   char c = i;
+  //   cout<<c<<endl;
+  // }
+
+  // for(char alpha = 'a'; alpha <='z'; alpha++)
+  // {
+  //   cout<<alpha<<endl;
+  // }
+
+  // sum of first 10 numbers
+
+  // int sum = 0;
+  // for (int i = 1; i <=10; i++)
+  // {
+  //   sum+=i;
+  // }
+  // cout<<sum;
+  
+
+  // sum of squares of 1st 10 numbers
+  // int sum = 0;
+  // for (int i = 1; i <=10; i++)
+  // {
+  //   sum = sum + i * i;
+  // }
+  // cout<<sum<<endl;
+  
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+
+  // for (int j = 1; j <=6; j++)
+  // {
+  // for (int i = 1; i <=5; i++)
+  // {
+  //   cout<<i<<" ";
+  // }
+  // cout<<endl;
+  // }
+  
+
+  // a b c d e
+  // a b c d e
+  // a b c d e
+  // a b c d e
+  // a b c d e
+
+  // for (int j = 1; j <=5; j++)
+  // {
+  // for (char i = 'a'; i <='e'; i++)
+  // {
+  //   cout<<i<<" ";
+  // }
+  // cout<<endl;
+  // }
+
+  //  10 to 1
+
+  // for (int i = 10; i >=1; i--)
+  // {
+  //   cout<<i<<endl;
+  // }
+
+  // Table program
+
+  // for (int i = 1; i <=10; i++)
+  // {
+  //   cout<<"7 X "<<i<<" = "<<7*i<<endl;
+  // }
+
+  // *****
+  // *****
+  // *****
+  // *****
+  // *****
+
+  // for (int i = 0; i <5; i++)
+  // {
+  //   for (int j = 0; j <5; j++)
+  //   {
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+    
+  // }
+
+  // *
+  // **
+  // ***
+  // ****
+  // *****
+
+  // for (int i = 1; i <6; i++)
+  // {
+  //   for (int j = 0; j <i; j++)
+  //   {
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // *****
+  // ****
+  // ***
+  // **
+  // *
+  
+  // for (int i = 5; i>0; i--)
+  // {
+  //   for (int j = 0; j <i; j++)
+  //   {
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // inverted right angle triangle
+
+  // for (int i = 4; i >=0; i--)
+  // {
+  //   for (int j = 0; j <i; j++)
+  //   {
+  //     cout<<" ";
+  //   }
+  //   for (int j = 0; j <5-i; j++)
+  //   {
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+  // }
+
+
+  // 1 2 3 4 5
+  // 6 7 8 9 10 upto 25
+
+
+  // for (int j = 0; j <21; j+=5)
+  // {
+  //   for (int i = 1+j; i <=5+j; i++)
+  // {
+  //   cout<<i<<" ";
+  // }
+  // cout<<endl;
+  // }
+
+  int num =1;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      cout<<num<<" ";
+      num++;
+    }
+    cout<<endl;
+    
+  }
+  
+  
+  
+  
+
+
+
+  
+  
+  
+  
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,5 @@
-
+for (int i = 97; i < 123; i++)
+  // {
+  //   char c = i;
+  //   cout<<c<<endl;
+  // }

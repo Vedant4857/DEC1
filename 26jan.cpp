0f3d6de915783e617bc4d5275ead9fa0759044a9
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2026-01-27 17:02:47 +0530

    started string

---
+++ 26jan.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // string s = "Vedant";
+  // cout<<s<<endl;
+
+  //size 
+  // cout<<s.size();
+  // cout<<s.length();
+
+  // string s1;
+  // // cin>>s1;
+  // getline(cin,s1);// For sentences with spaces
+  // cout<<s1;
+
+  //different operations
+
+  string s1 = "Vedant ";
+  string s2 = "Shekhar";
+  string s3 = s1 + s2;
+  
+
+  //character can be pushed
+
+  // s3.push_back('r');
+
+  //  can be popped back
+  // s3.pop_back();
+  // cout<<s3;
+
+  // char c = 'a'+5;
+  // cout<<c;
+
+  // char c[6] = {'V','e','d','a','n','t'};
+  // cout<<c;
+
+  // char c[] = "Vedant";
+  // cout<<c;
+
+  string s = "Vedant";
+  cout<<s.size();
+
+
+
+
+
+    
+  return 0;
+}

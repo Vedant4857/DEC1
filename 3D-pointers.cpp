454da6f1604c74f930af157ee7a9cb4ae06f2e88
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2026-02-14 14:12:13 +0530

    comments

--- 3D-pointers.cpp
+++ 3D-pointers.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main()
 {
+  //arr is a pointer that points to a pointer that points to a pointer that points to an int array
   int ***arr = new int**[2];
 
+  //making the the first array pointer point to another array of pointer of size 2
   for(int i =0;i<2;i++){
     arr[i] = new int*[2];
   }
 
+  //making the second array of pointer point to an array of integer of size 2
   for(int i =0;i<2;i++){
     for(int j =0;j<2;j++){
       arr[i][j] = new int[2];
     }
   }
-
+ 
+  //intializing the 3d array
   for(int i =0;i<2;i++){
     for(int j =0;j<2;j++){
       for(int k =0;k<2;k++){
@@ -23,6 +27,7 @@ int main()
     }
   }
 
+  //printing the values
   for(int i =0;i<2;i++){
     for(int j =0;j<2;j++){
       for(int k =0;k<2;k++){
@@ -33,6 +38,7 @@ int main()
     cout<<endl;
   }
 
+  //freeing up the space
   for(int i=0;i<2;i++){
     for(int j=0;j<2;j++){
         delete[] arr[i][j];
@@ -40,9 +46,6 @@ int main()
     delete[] arr[i];
 }
   delete[] arr;
-
-
-
-
+  
   return 0;
 }

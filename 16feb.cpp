793fd4724b31916ab61145811c246317ceb6496d
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2026-02-16 22:07:39 +0530

    changes

--- 16feb.cpp
+++ 16feb.cpp
@@ -4,8 +4,8 @@
 //   if(index==n) return;
 //   cout<<arr[index]<<endl;
 //   print(arr,index+1,n);
-
 // }
+
 // int main()
 // {
 //   int arr[6] = {1,2,3,4,5,6};
@@ -57,17 +57,55 @@
 
 
 
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int index){
+//   if(index==-1) return;
+//   cout<<arr[index]<<endl;
+//   print(arr,index-1);
+// }
+// int main()
+// {
+//   int arr[6] = {1,2,3,4,5,6};
+//   print(arr,5);
+
+//   return 0;
+// }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int index){
+//   if(index==-1) return;
+//   print(arr,index-1);
+//   cout<<arr[index]<<endl;
+// }
+// int main()
+// {
+//   int arr[6] = {1,2,3,4,5,6};
+//   print(arr,5);
+
+//   return 0;
+// }
+
+
+
+
 #include <iostream>
 using namespace std;
-void print(int arr[],int index){
-  if(index==-1) return;
-  cout<<arr[index]<<endl;
-  print(arr,index-1);
+
+int sum(int arr[],int index,int n){
+  if(index==n){
+    return 0;
+  }
+  return (arr[index] + sum(arr,index+1,n));
 }
 int main()
 {
   int arr[6] = {1,2,3,4,5,6};
-  print(arr,5);
+  cout<<sum(arr,0,6);
 
   return 0;
 }

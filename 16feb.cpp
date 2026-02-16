45517d5c0b163543c155bf2962a7204da5c6e9e5
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2026-02-16 21:33:26 +0530

    more changes

--- 16feb.cpp
+++ 16feb.cpp
@@ -1,15 +1,73 @@
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int index,int n){
+//   if(index==n) return;
+//   cout<<arr[index]<<endl;
+//   print(arr,index+1,n);
+
+// }
+// int main()
+// {
+//   int arr[6] = {1,2,3,4,5,6};
+//   print(arr,0,6);
+
+//   return 0;
+// }
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int index,int n){
+//   if(n==index) return;
+//   cout<<arr[n-1]<<endl;
+//   print(arr,index,n-1);
+
+// }
+// int main()
+// {
+//   int arr[6] = {1,2,3,4,5,6};
+//   print(arr,0,6);
+
+//   return 0;
+// }
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int index,int n){
+//   if(index==n) return;
+//   print(arr,index+1,n);
+//   cout<<arr[index]<<endl;
+// }
+// int main()
+// {
+//   int arr[6] = {1,2,3,4,5,6};
+//   print(arr,0,6);
+
+//   return 0;
+// }
+
+
+
+
 #include <iostream>
 using namespace std;
-void print(int arr[],int index,int n){
-  if(index==n) return;
+void print(int arr[],int index){
+  if(index==-1) return;
   cout<<arr[index]<<endl;
-  print(arr,index+1,n);
-
+  print(arr,index-1);
 }
 int main()
 {
   int arr[6] = {1,2,3,4,5,6};
-  print(arr,0,6);
+  print(arr,5);
 
   return 0;
 }

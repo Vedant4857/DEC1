190319f38a431745d15245f5f219f42028e7c854
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2026-03-31 22:34:05 +0530

    Pre,In,Post traversals

--- 31marchTrees.cpp
+++ 31marchTrees.cpp
@@ -12,6 +12,37 @@ class Node{
     left = right = NULL;
   }
 };
+
+void preorder(Node *root){
+  if(!root){
+    return;
+  }
+
+  cout<<root->data<<" ";
+  preorder(root->left);
+  preorder(root->right);
+}
+
+void inorder(Node *root){
+  if(!root){
+    return;
+  }
+
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+
+void postorder(Node *root){
+  if(!root){
+    return;
+  }
+
+  postorder(root->left);
+  postorder(root->right);
+  cout<<root->data<<" ";
+}
+
 int main()
 {
   int x,leftvalue,rightvalue;
@@ -41,5 +72,17 @@ int main()
       q.push(temp->right);
     }
   }
+  
+  cout<<"Preorder Traversal:"<<" ";
+  preorder(root);
+  cout<<endl;
+
+  cout<<"Inorder Traversal:"<<" ";
+  inorder(root);
+  cout<<endl;
+
+  cout<<"Postorder Traversal:"<<" ";
+  postorder(root);
+  cout<<endl;
   return 0;
 }

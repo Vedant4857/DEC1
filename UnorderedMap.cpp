9b08281ac23e4559e2c74435bd9ba74c88d301bb
Vedant4857 <vedantshekharb1@gmail.com>
2026-05-06 22:26:11 +0530

    UnorderedMap

---
+++ UnorderedMap.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    unordered_multimap<string,int> m;
+
+    //how to insert
+    // m["vedant"] = 103;
+    // m["John"] = 122;
+    // m["david"] = 344;
+
+    m.insert({"Harsh",234});
+
+    // cout<<m["Harsh"]<<" ";
+
+    // for(auto x : m){
+    //     cout<<x.first<<" "<<x.second<<endl;
+    // }
+
+    auto it = m.find("Harsh");
+
+    if(it!=m.end()){
+        cout<<"Found";
+    }
+    else{
+        cout<<"Not found";
+    }
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    return 0;
+}

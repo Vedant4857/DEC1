38995a53ad3b00965d90d45c6c708e3b8cdb5347
Vedant4857 <vedantshekharb1@gmail.com>
2026-05-06 22:16:15 +0530

    Maps-ordered

---
+++ Maps.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    // map<string,int> m;
+
+    // //how to insert
+    // m["vedant"] = 103;
+    // m["John"] = 122;
+    // m["david"] = 344;
+
+    // m.insert({"Harsh",234});
+
+    // // cout<<m["Harsh"]<<" ";
+
+    // for(auto x : m){
+    //     cout<<x.first<<" "<<x.second<<endl;
+    // }
+
+
+    
+
+
+
+
+
+
+
+    return 0;
+}

b43de1a9ea090aa241bcc40b9558b667e3e74a13
Vedant4857 <vedantshekharb1@gmail.com>
2026-05-27 19:34:52 +0530

    2

--- oops1.cpp
+++ oops1.cpp
@@ -9,23 +9,54 @@ public:
     int rollNumber;
     string college;
 
-    // student(string n, int a, int rn, string c)
-    // {
-    //     name = n;
-    //     age = a;
-    //     rollNumber = rn;
-    //     college = c;
-    // }
-    
+    // parameterized constrictor
+    student(string n, int a, int rn, string c)
+    {
+        name = n;
+        age = a;
+        rollNumber = rn;
+        college = c;
+    }
+
+    // default constructore
+    student()
+    {
+    }
+
+    // behaviours of an object --> functions -->methods
+    void markAttendance()
+    {
+        cout << name << " is present" << endl;
+    }
+    void print()
+    {
+        cout << name << " " << age << " " << rollNumber << " " << college << endl;
+    }
+};
+class calculator
+{
+    int x, y;
+    int add()
+    {
+        return 0;
+    }
+    int subtract()
+    {
+        return 0;
+    }
 };
 int main()
 {
-    // stack & heap
-    student s1;
+    // stack
+    student s1("Vedant", 21, 103, "MU");
     // s1.name = "Vedant";
     // s1.age = 21;
     // s1.rollNumber = 103;
     // s1.college = "MU";
+    s1.print();
+    s1.markAttendance();
 
-    cout << s1.name << " " << s1.age << " " << s1.rollNumber << " " << s1.college;
+    // Heap
+    student *s2 = new student("Virat", 38, 204, "DU");
+    cout << s2->name << " " << s2->age << " " << s2->rollNumber << " " << s2->college << endl;
 }
